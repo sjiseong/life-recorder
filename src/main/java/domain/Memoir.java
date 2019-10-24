1e05bfa8f15548a43dc2dc3d1f4225f162922108
jiseong <gseong0906@gmail.com>
2019-10-24 09:58:19 +0900

    domain

---
+++ src/main/java/domain/Memoir.java
@@ -0,0 +1,33 @@
+package domain;
+
+public class Memoir {
+
+	private String title, content, write_time;
+	private int id;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWrite_time() {
+		return write_time;
+	}
+	public void setWrite_time(String write_time) {
+		this.write_time = write_time;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+}
---
+++ src/main/java/domain/Record.java
@@ -0,0 +1,39 @@
+package domain;
+
+public class Record {
+
+	private String title, content, write_time;
+	private int id, type;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWrite_time() {
+		return write_time;
+	}
+	public void setWrite_time(String write_time) {
+		this.write_time = write_time;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+}
